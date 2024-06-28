10f2a2c841f7d1be11b6d70d549c432a9952cb60
HECyeah <hannahecarter@gmail.com>
2024-06-28 22:33:23 +0100

    change

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,11 +6,11 @@ export function NavBar() {
     return (
         <div className="pink">
             <h1>Hull & Home</h1>
-            <nav className="navNav">
+            <nav className="nav">
                 <ul className="nav">
                     <li><Link href="/" className="nav">Home</Link></li>
                     <li><Link href="/add-plant" className="nav">Add a Plant</Link></li>
-                    <li><Link href="/comments" className="nav">Discussion Board</Link></li>
+                    <li><Link href="/" className="nav">Discussion Board</Link></li>
                 </ul>
             </nav>
         </div>
